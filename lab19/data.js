fa8584acad0ef1ce3dd49bf9f2f0043e00deecaa
ssarabun2022-pixel <ssarabun2022@itcollege.lviv.ua>
2026-04-09 23:33:08 +0300

    u

--- lab19/data.js
+++ lab19/data.js
@@ -7,16 +7,6 @@ export const todos = [
         tag: "Навчання",
         deadline: "2024-12-01",
         createdAt: 1700000000,
-        isDone: true
-    },
-    {
-        id: "2",
-        userId: 40,
-        title: "Створити власний API",
-        description: "Написати сервер, що віддає JSON",
-        tag: "Робота",
-        deadline: "2024-12-05",
-        createdAt: 1700005000,
-        isDone: false
+        isDone: true,
     }
 ];
