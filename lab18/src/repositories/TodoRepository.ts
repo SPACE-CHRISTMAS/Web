6274de04c22e1ed2cae14538cba2caec55aba596
ssarabun2022-pixel <ssarabun2022@itcollege.lviv.ua>
2026-07-13 14:00:37 +0300

    u

--- lab18/src/repositories/TodoRepository.ts
+++
@@ -1,8 +0,0 @@
-import { Todo } from "../scss/models/Todo";
-
-export interface TodoRepository {
-    getAll(): Promise<Todo[]>;
-    add(todo: Todo): Promise<Todo>;
-    update(id: string, data: Partial<Todo>): Promise<Todo>;
-    remove(id: string): Promise<void>;
-}
